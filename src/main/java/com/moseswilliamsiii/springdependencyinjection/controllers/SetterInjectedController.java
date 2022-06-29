e0802cd5d453874ed736d6e65de873bac9f2cd5f
mowilliamsiii <moseswilliamsiii99@gmail.com>
2022-06-28 21:41:04 -0400

    updated services

--- src/main/java/com/moseswilliamsiii/springdependencyinjection/controllers/SetterInjectedController.java
+++ src/main/java/com/moseswilliamsiii/springdependencyinjection/controllers/SetterInjectedController.java
@@ -2,10 +2,12 @@ package com.moseswilliamsiii.springdependencyinjection.controllers;
 
 import com.moseswilliamsiii.springdependencyinjection.services.GreetingService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Controller;
 
 @Controller
 public class SetterInjectedController {
+    @Qualifier("setterInjectedGreetingService")
     @Autowired
     private GreetingService greetingService;
 
